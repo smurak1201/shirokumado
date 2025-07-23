70bd4da00900858cbf3a2364e06f61e2919c7cab
smurak1201 <skip.to.my.lou.1201@gmail.com>
2025-07-23 09:49:03 +0900

    ヘッダーにロゴ画像を追加し、タイトルのスタイルを調整

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,6 @@
 <header class="site-header">
-    <h1>白熊堂</h1>
+    <h1 style="margin:0;">
+        <img src="{{ asset('storage/images/tenpo_icon.jpg') }}" alt="白熊堂ロゴ" style="height:60px;">
+    </h1>
     <!-- ナビゲーションなど追加可能 -->
 </header>
