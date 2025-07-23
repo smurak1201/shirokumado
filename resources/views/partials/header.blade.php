0797323c25ec55e9fbba1a4b85acc0375e28b850
smurak1201 <skip.to.my.lou.1201@gmail.com>
2025-07-23 10:05:54 +0900

    ヘッダーのスタイルを調整し、ナビゲーションを追加

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,13 @@
-<header class="site-header">
+<header class="site-header" style="display: flex; align-items: center; gap: 20px;">
     <h1 style="margin:0;">
-        <img src="{{ asset('storage/images/tenpo_icon.jpg') }}" alt="白熊堂ロゴ" style="height:60px;">
+        <a href="{{ url('/') }}">
+            <img src="{{ asset('storage/images/tenpo_icon.jpg') }}" alt="白熊堂ロゴ" style="height:60px;">
+        </a>
     </h1>
+    <nav>
+        <a href="https://www.instagram.com/shirokumado2021/#" target="_blank" rel="noopener" title="Instagram">
+            <img src="{{ asset('storage/images/Instagram_Glyph_Black-scaled.png') }}" alt="Instagram" style="height:32px; width:auto; vertical-align:middle;">
+        </a>
+    </nav>
     <!-- ナビゲーションなど追加可能 -->
 </header>
