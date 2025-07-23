c29154215e7779d9de36ce611b26c5fe4b7e637e
smurak1201 <skip.to.my.lou.1201@gmail.com>
2025-07-23 10:53:06 +0900

    フッターにアイコンセクションを追加し、ロゴとInstagramリンクのスタイルを適用

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,12 @@
 <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
 <footer class="site-footer">
+    <div class="footer-icons">
+        <a href="{{ url('/') }}">
+            <img class="footer-logo" src="{{ asset('storage/images/tenpo_icon.jpg') }}" alt="白熊堂ロゴ">
+        </a>
+        <a href="https://www.instagram.com/shirokumado2021/#" target="_blank" rel="noopener" title="Instagram">
+            <img class="footer-instagram" src="{{ asset('storage/images/Instagram_Glyph_Black-scaled.png') }}" alt="Instagram">
+        </a>
+    </div>
     <small>&copy; 2025 白熊堂 All rights reserved.</small>
 </footer>
