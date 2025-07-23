c7ccff16755ea2a6b6a9e2e11565ac6554da3bb6
smurak1201 <skip.to.my.lou.1201@gmail.com>
2025-07-23 15:35:07 +0900

    Tailwind CSSのsafelistからgapクラスを削除

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,6 @@ module.exports = {
         './resources/js/**/*.js',
         './resources/css/**/*.css',
     ],
-    safelist: [
-        'gap-8',
-        'gap-4',
-    ],
     theme: {
         extend: {},
     },
