394dba1c37fd1a1109d1094b7bef0e64dc44311f
smurak1201 <skip.to.my.lou.1201@gmail.com>
2025-07-23 11:04:35 +0900

    フッターに情報セクションを追加し、テキストのスタイルを調整

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -8,5 +8,11 @@
             <img class="footer-instagram" src="{{ asset('storage/images/Instagram_Glyph_Black-scaled.png') }}" alt="Instagram">
         </a>
     </div>
+    <div class="footer-info">
+        <p>神奈川県川崎市川崎区小川町4-1<br>ラチッタデッラ マッジョーレ1F</p>
+        <p>営業時間：11:00～21:00(L.O.20:00)</p>
+        <p>070-9157-3772</p>
+        <p>通年営業 月1回不定休</p>
+    </div>
     <small>&copy; 2025 白熊堂 All rights reserved.</small>
 </footer>
