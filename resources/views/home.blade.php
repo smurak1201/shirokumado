f95a8409ce246933fe346432b825ac521cf99b37
smurak1201 <skip.to.my.lou.1201@gmail.com>
2025-07-23 10:12:03 +0900

    CSSファイルを新規作成し、ヘッダー、フッター、コンテンツ用のスタイルを追加。不要なスタイルを削除。

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,7 @@
 
 @section('title', '白熊堂')
 
+<link rel="stylesheet" href="{{ asset('css/content.css') }}">
 @section('content')
 <div class="hero-container">
     <img class="hero-image" src="{{ asset('storage/images/tenpo_gaikan.jpg') }}" alt="店舗外観">
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="{{ asset('css/footer.css') }}">
 <footer class="site-footer">
     <small>&copy; 2025 白熊堂 All rights reserved.</small>
 </footer>
