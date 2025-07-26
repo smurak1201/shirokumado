ab5273832b25c490765f9d85e20c5a5e298d927e
smurak1201 <skip.to.my.lou.1201@gmail.com>
2025-07-26 15:17:28 +0900

    Instagram投稿一覧ページのルート定義を整理

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,8 @@ use App\Http\Controllers\GetImagesController;
 
 Route::get('/', [GetImagesController::class, 'index']);
 
+// Instagram投稿一覧ページ
+Route::view('/instagram', 'instagram')->name('instagram');
 
 // FAQページ
 Route::view('/faq', 'faq')->name('faq');
-
-// Instagram投稿一覧ページ
-Route::view('/instagram', 'instagram')->name('instagram');
