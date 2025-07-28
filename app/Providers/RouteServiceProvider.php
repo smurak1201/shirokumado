debb859195405c82e867516c1bda7867607c3031
smurak1201 <skip.to.my.lou.1201@gmail.com>
2025-07-28 11:35:49 +0900

    fix: 修正された名前空間のインポートを更新

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Foundation\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
