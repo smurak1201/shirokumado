31a42548340092b7309ac270bd46832e785af633
smurak1201 <skip.to.my.lou.1201@gmail.com>
2025-07-28 15:09:36 +0900

    fix: base URLを設定し、正しいパスを指定

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -6,6 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
+    base: '/shirokumado/dashboard/',
     plugins: [react(), tsconfigPaths()],
     define: {
       'import.meta.env.VITE_APP_URL': JSON.stringify(env.APP_URL),
