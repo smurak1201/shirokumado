ca9963a275eec074cfb6c0cba8ac7c3b8fb2828c
smurak1201 <skip.to.my.lou.1201@gmail.com>
2025-07-29 14:49:03 +0900

    feat: Viteの設定でベースパスを'/'に変更

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    base: '/shirokumado/',
+    base: '/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
